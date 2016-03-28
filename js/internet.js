3eed642bc7ff75660796a07ab38fc89c92843e1c
manachait <manachai_toa@utcc.ac.th>
2016-03-28 14:10:40 +0700

    Ver1

---
+++ js/internet.js
@@ -0,0 +1,9 @@
+function openweb1(){
+	window.open('http://www.utcc.ac.th', '_blank', 'location=yes');
+}
+function openweb2(){
+	window.open('http://www.utcc.ac.th', '_self', 'location=no');
+}
+function openweb3(){
+	window.open('http://www.utcc.ac.th', '_system', 'location=no');
+}
